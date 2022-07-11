3fc68d832a9440d369dd429942e994788708229f
Hubert Zając <hubert.zajac92@gmail.com>
2022-07-11 17:03:17 +0200

    create error handling and connection with database

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { json } from "express";
 import cors from 'cors';
 import 'express-async-errors';
+import { handleError } from "./utils/handleError";
 
 const app = express();
 
@@ -9,6 +10,8 @@ app.use(cors({
 }));
 app.use(json());
 
+app.use(handleError);
+
 app.listen(3001, '0.0.0.0', () => {
     console.log('server is listening on port http://localhost:3001');
-}) 
+});
---
+++ types/training/training.ts
@@ -0,0 +1,3 @@
+export interface Training {
+    x: number;
+}
